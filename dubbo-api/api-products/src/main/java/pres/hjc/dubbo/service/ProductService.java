37f1996a60546840498b280ab2e25b2cd5fc47d1
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-04-22 14:32:28 +0800

    基础框架 + entity + api + service

---
+++ dubbo-api/api-products/src/main/java/pres/hjc/dubbo/service/ProductService.java
@@ -0,0 +1,13 @@
+package pres.hjc.dubbo.service;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author HJC
+ * @version 1.0
+ * To change this template use File | Settings | File Templates.
+ * @date 2020/4/22
+ * @time 14:15
+ */
+public interface ProductService {
+}
---
+++ dubbo-service/service-products/src/main/java/pres/hjc/dubbo/impl/ProductsServiceImpl.java
@@ -0,0 +1,16 @@
+package pres.hjc.dubbo.impl;
+
+import pres.hjc.dubbo.service.ProductService;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author HJC
+ * @version 1.0
+ * To change this template use File | Settings | File Templates.
+ * @date 2020/4/22
+ * @time 14:22
+ */
+public class ProductsServiceImpl implements ProductService {
+
+}
